0e2c5402713e6b63dcd7e815e7da983bb5f1d930
ALegaz <alvarlegaz@gmail.com>
2024-06-27 21:05:58 +0200

    Update

--- app/security_toolbox.py
+++ app/security_toolbox.py
@@ -2,6 +2,8 @@ import jwt
 import datetime
 import hashlib
 
+secret_key = 'mi_clave_secreta'
+
 def get_sha256_signature(data):
     hash_object = hashlib.sha256()
     hash_object.update(data.encode('utf-8'))
@@ -12,13 +14,11 @@ def get_jwt_token(user, time_life_minutes):
     'user_id': user,
     'exp': datetime.datetime.now() + datetime.timedelta(minutes=time_life_minutes) 
     }
-    secret_key = 'mi_clave_secreta'
     encoded_token = jwt.encode(payload, secret_key, algorithm='HS256')
     return encoded_token
 
 def decode_jwt_token(token):
     try:
-        secret_key = 'mi_clave_secreta'
         decoded_payload = jwt.decode(token, secret_key, algorithms=["HS256"])
         return decoded_payload
     
