cc9693d6f97b6d694d002aede85ebb6120ca6867
Alvar Legaz Aparicio <alvarlegaz@gmail.com>
2024-07-31 13:01:57 +0200

    Update

--- Deploy_services_linux.sh
+++ Deploy_services_linux.sh
@@ -4,8 +4,8 @@ echo Create POD and map sercices ports
 podman pod create --name users_services_pod -p 3000:3000 -p 27017:27017
 
 echo Create login_service container in pod using image container
-podman run -d --pod login_service --name login_service localhost/login_service:latest
-podman run -d --pod login_service --name user_database docker.io/bitnami/mongodb:latest
+podman run -d --pod users_services_pod --name login_service localhost/login_service:latest
+podman run -d --pod users_services_pod --name user_database docker.io/bitnami/mongodb:latest
 
 echo Show deploy pod and container
 podman ps --pod
