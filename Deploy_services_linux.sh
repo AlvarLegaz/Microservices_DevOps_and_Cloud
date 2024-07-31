4f429e8aa19051ab1941ba6faab8475a33e847f8
Alvar Legaz Aparicio <alvarlegaz@gmail.com>
2024-07-31 12:57:18 +0200

    Update Deploy_services_linux.sh

--- Deploy_services_linux.sh
+++ Deploy_services_linux.sh
@@ -1,6 +1,11 @@
-echo Deploy Services
+echo Deploying Services Script ...
 
-echo Deploy API LOGIN at PORT 3000
-export FLASK_APP=app/services/login/api_login.py
-python3 -m flask --debug run -h 0.0.0.0 -p 3000 &
-pause()
+echo Create POD and map sercices ports
+podman pod create --name users_services_pod -p 3000:3000 -p 27017:27017
+
+echo Create login_service container in pod using image container
+podman run -d --pod login_service --name login_service localhost/login_service:latest
+podman run -d --pod login_service --name user_database docker.io/bitnami/mongodb:latest
+
+echo Show deploy pod and container
+podman ps --pod
