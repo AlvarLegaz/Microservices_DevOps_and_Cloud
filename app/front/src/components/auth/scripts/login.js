126745694a50af94dca891bc561ff63a24b6f813
Alvar Legaz Aparicio <alvarlegaz@gmail.com>
2024-08-07 13:59:36 +0200

    Login front  v1.0.0

--- app/front/src/components/auth/scripts/login.js
+++ app/front/src/components/auth/scripts/login.js
@@ -1,21 +1,24 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const username = document.getElementById('username').value;
+    const user= document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    const data = { username, password };
+    const data = { user, password };
+    console.log('body:', data);		
 
     fetch('http://192.168.18.64:3000/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
+	mode: 'cors', // Ensure this is set to 'cors'
         body: JSON.stringify(data)
+	
     })
     .then(response => response.json())
     .then(data => {
-        console.log('Success:', data);
+        console.log('Response:', data);
         // Aquí puedes manejar la respuesta del servidor
     })
     .catch((error) => {
