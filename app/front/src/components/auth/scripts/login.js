d37815a7733cdbc6f497971320c02da750062047
Alvar Legaz Aparicio <alvarlegaz@gmail.com>
2024-09-15 16:46:09 +0200

    Revert "Update front"
    This reverts commit aed0a84bda37f469350d336e72e58a52fea6970d.

--- app/front/src/components/auth/scripts/login.js
+++ app/front/src/components/auth/scripts/login.js
@@ -14,22 +14,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         mode: 'cors',
         body: JSON.stringify(data)
     })
-    .then(response => {
-        if (!response.ok) {
-            return response.json().then(errorData => {
-                throw new Error(JSON.stringify(errorData));
-            });
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Login Success:', data);
-        alert('Goes to selected url');
-    })
-    .catch((error) => {
-        console.error('Login Error:', error);
-        showMessage('Error en el login:',error.message);
-    });
+    .then(response => response.json())
+    .then(data => console.log('Login Success:', data))
+    .catch((error) => console.error('Login Error:', error));
 });
 
 document.getElementById('registerForm').addEventListener('submit', function(event) {
@@ -48,51 +35,22 @@ document.getElementById('registerForm').addEventListener('submit', function(even
         mode: 'cors',
         body: JSON.stringify(data)
     })
-    .then(response => {
-        if (!response.ok) {
-            return response.json().then(errorData => {
-                throw new Error(JSON.stringify(errorData));
-            });
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Register completed: ', data);
-        showMessage('Success!','Register completed');
-    })
-    .catch((error) => {
-        console.error('Registration error Error:', error);
-        showMessage('Registration error:', error.message);
-    });
-});
-
-function showMessage(title, message) {
-    const messageContainer = document.getElementById('message-container');
-    const messageTitle = document.getElementById('message-title');
-    const messageContent = document.getElementById('message-content');
-    
-    messageTitle.textContent = title;
-    messageContent.textContent = message;
-    messageContainer.style.display = 'block';
-}
-
-document.getElementById('close-message').addEventListener('click', function() {
-    const messageContainer = document.getElementById('message-container');
-    messageContainer.style.display = 'none';
+    .then(response => response.json())
+    .then(data => console.log('Register Success:', data))
+    .catch((error) => console.error('Register Error:', error));
 });
 
 document.getElementById('showRegisterForm').addEventListener('click', function() {
-    const loginContainer = document.getElementById('login-container');
-    const registerContainer = document.getElementById('register-container');
+    const loginContainer = document.querySelector('.login-container .row:nth-child(1)');
+    const registerContainer = document.getElementById('registerContainer');
 
-    loginContainer.style.display = 'none';
-    registerContainer.style.display = 'block'; 
+    if (registerContainer.style.display === 'none') {
+        registerContainer.style.display = 'block';
+        loginContainer.style.display = 'none';
+    } else {
+        registerContainer.style.display = 'none';
+        loginContainer.style.display = 'block';
+    }
 });
 
-document.getElementById('showLoginForm').addEventListener('click', function() {
-    const loginContainer = document.getElementById('login-container');
-    const registerContainer = document.getElementById('register-container');
 
-    registerContainer.style.display = 'none';
-    loginContainer.style.display = 'block';
-});
--- app/tools/security_toolbox.py
+++ app/tools/security_toolbox.py
@@ -3,39 +3,37 @@ import datetime
 import hashlib
 
 
-class SecurityToolbox:
+class security_toolbox:
 
     def __init__(self, secret_key):
         self.secret_key = secret_key
+
 
     def get_sha256_signature(self, data):
         hash_object = hashlib.sha256()
         hash_object.update(str(data).encode('utf-8'))
         return hash_object.hexdigest()
 
+
     def get_jwt_token(self, user, time_life_minutes):
         try:
-            user_str = str(user)
+            user_str=str(user)
             payload = {
-                'user_id': user_str,
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=time_life_minutes)
+            'user_id': user_str,
+            'exp': datetime.datetime.now() + datetime.timedelta(minutes=time_life_minutes) 
             }
             encoded_token = jwt.encode(payload, self.secret_key, algorithm='HS256')
             return encoded_token
         except Exception as e:
-            raise Exception("SecurityToolbox exception - Fail encoding JWT token: " + str(e))
+            raise Exception("Security_toobox exception- Fail encoding JWT token: "+str(payload)+ str(e))
+
 
     def decode_jwt_token(self, token):
         try:
             decoded_payload = jwt.decode(token, self.secret_key, algorithms=["HS256"])
             return decoded_payload
+        
         except jwt.ExpiredSignatureError:
-            raise Exception("El token ha expirado.")
+            print("El token ha expirado.")
         except jwt.DecodeError:
-            raise Exception("Error al decodificar el token.")      
-
-if __name__ == '__main__':
-    # Uso de la clase
-    my_toolbox = SecurityToolbox("sdasdasdas")
-
-    print(my_toolbox.get_jwt_token('alvar',10))
+            print("Error al decodificar el token.")    
