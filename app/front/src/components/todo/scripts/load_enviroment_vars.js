b27bcd92f79e20fb94f2b7e4c6c75add495b529b
Álvar Legaz Aparicio <116595540+AlvarLegaz@users.noreply.github.com>
2024-10-04 14:56:16 +0200

    Update load_enviroment_vars.js

--- app/front/src/components/todo/scripts/load_enviroment_vars.js
+++ app/front/src/components/todo/scripts/load_enviroment_vars.js
@@ -1,7 +1,11 @@
-var user = "alvar";
+var user = "xxx";
+var token_jwt = "xxx";
 var base_url="http://127.0.0.1:3001"
 
 function loadEnviroment(){
+
+    var user = localStorage.getItem("user");
+    var token_jwt = localStorage.getItem("token_jwt");
 
     var miDiv = document.getElementById("user_workspace");
     miDiv.innerText = `${user} Workspace`;
